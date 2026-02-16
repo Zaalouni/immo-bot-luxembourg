b465696157990f3032b89af878922807cf9147f6
zaalouni <nsaibi@gmail.com>
2026-02-16 20:54:12 +0100

    dashboard: mise a jour 2026-02-16 20:54

--- dashboards/data/stats.js
+++ dashboards/data/stats.js
@@ -1,30 +1,20 @@
-// Genere le 16/02/2026 20:27
+// Genere le 16/02/2026 20:54
 const STATS = {
-  "total": 48,
-  "avg_price": 2290,
+  "total": 38,
+  "avg_price": 2269,
   "min_price": 1700,
   "max_price": 2800,
-  "avg_surface": 122,
-  "cities": 38,
+  "avg_surface": 114,
+  "cities": 29,
   "sites": {
-    "Nextimmo.lu": 13,
-    "VIVI.lu": 7,
-    "Immotop.lu": 7,
-    "Newimmo.lu": 3,
+    "Nextimmo.lu": 11,
+    "Newimmo.lu": 2,
+    "VIVI.lu": 4,
     "Luxhome.lu": 11,
-    "Athome.lu": 7
+    "Immotop.lu": 5,
+    "Athome.lu": 5
   },
   "by_city": [
-    {
-      "city": "Luxembourg",
-      "count": 3,
-      "avg_price": 2050
-    },
-    {
-      "city": "Junglinster",
-      "count": 2,
-      "avg_price": 2375
-    },
     {
       "city": "Schengen",
       "count": 2,
@@ -36,6 +26,16 @@ const STATS = {
       "avg_price": 2375
     },
     {
+      "city": "Kirchberg",
+      "count": 2,
+      "avg_price": 2650
+    },
+    {
+      "city": "Luxembourg",
+      "count": 2,
+      "avg_price": 2125
+    },
+    {
       "city": "Perl",
       "count": 2,
       "avg_price": 1875
@@ -44,6 +44,26 @@ const STATS = {
       "city": "Mondorf-les-Bains",
       "count": 1,
       "avg_price": 2500
+    },
+    {
+      "city": "Bettembourg",
+      "count": 1,
+      "avg_price": 2500
+    },
+    {
+      "city": "Bettendorf",
+      "count": 1,
+      "avg_price": 1800
+    },
+    {
+      "city": "Oberanven",
+      "count": 1,
+      "avg_price": 2800
+    },
+    {
+      "city": "Differdange",
+      "count": 1,
+      "avg_price": 1950
     },
     {
       "city": "Luxembourg-Cessange",
@@ -71,69 +91,9 @@ const STATS = {
       "avg_price": 2000
     },
     {
-      "city": "Bonnevoie",
-      "count": 1,
-      "avg_price": 2650
-    },
-    {
-      "city": "Habscht",
-      "count": 1,
-      "avg_price": 1900
-    },
-    {
-      "city": "Bettembourg",
-      "count": 1,
-      "avg_price": 2500
-    },
-    {
-      "city": "Bettendorf",
-      "count": 1,
-      "avg_price": 1800
-    },
-    {
-      "city": "Oberanven",
-      "count": 1,
-      "avg_price": 2800
-    },
-    {
-      "city": "Differdange",
-      "count": 1,
-      "avg_price": 1950
-    },
-    {
-      "city": "Luxembourg-Rollingergrund",
-      "count": 1,
-      "avg_price": 2500
-    },
-    {
-      "city": "Luxembourg-Gasperich",
-      "count": 1,
-      "avg_price": 2350
-    },
-    {
-      "city": "Luxembourg Limpertsberg",
-      "count": 1,
-      "avg_price": 2500
-    },
-    {
-      "city": "Luxembourg Neudorf",
+      "city": "Luxembourg Gare",
       "count": 1,
       "avg_price": 2000
-    },
-    {
-      "city": "Grevenmacher",
-      "count": 1,
-      "avg_price": 2500
-    },
-    {
-      "city": "Bech",
-      "count": 1,
-      "avg_price": 2800
-    },
-    {
-      "city": "Rippig",
-      "count": 1,
-      "avg_price": 2800
     },
     {
       "city": "Echternach",
@@ -141,9 +101,19 @@ const STATS = {
       "avg_price": 2795
     },
     {
+      "city": "Bech",
+      "count": 1,
+      "avg_price": 2800
+    },
+    {
       "city": "Gasperich",
       "count": 1,
       "avg_price": 2500
+    },
+    {
+      "city": "Bonnevoie",
+      "count": 1,
+      "avg_price": 2650
     },
     {
       "city": "Strassen",
@@ -151,19 +121,9 @@ const STATS = {
       "avg_price": 2230
     },
     {
-      "city": "Gare",
-      "count": 1,
-      "avg_price": 1700
-    },
-    {
       "city": "Hollerich",
       "count": 1,
       "avg_price": 2450
-    },
-    {
-      "city": "Kirchberg",
-      "count": 1,
-      "avg_price": 2500
     },
     {
       "city": "Esch",
@@ -171,24 +131,19 @@ const STATS = {
       "avg_price": 2750
     },
     {
-      "city": "Sanem",
-      "count": 1,
-      "avg_price": 2050
-    },
-    {
       "city": "Bertrange",
       "count": 1,
       "avg_price": 2500
     },
     {
-      "city": "Nagem",
+      "city": "Habscht",
       "count": 1,
-      "avg_price": 2600
+      "avg_price": 1900
     },
     {
-      "city": "Luxembourg-Kirchberg",
+      "city": "Junglinster",
       "count": 1,
-      "avg_price": 2800
+      "avg_price": 1950
     },
     {
       "city": "Luxembourg-Centre Ville",
@@ -208,16 +163,16 @@ const STATS = {
   ],
   "by_price_range": {
     "< 1500": 0,
-    "1500 - 2000": 11,
-    "2000 - 2500": 17,
-    "> 2500": 20
+    "1500 - 2000": 9,
+    "2000 - 2500": 15,
+    "> 2500": 14
   }
 };
 const SITE_COLORS = {
   "Nextimmo.lu": "#FF6384",
-  "VIVI.lu": "#36A2EB",
-  "Immotop.lu": "#FFCE56",
-  "Newimmo.lu": "#4BC0C0",
-  "Luxhome.lu": "#9966FF",
+  "Newimmo.lu": "#36A2EB",
+  "VIVI.lu": "#FFCE56",
+  "Luxhome.lu": "#4BC0C0",
+  "Immotop.lu": "#9966FF",
   "Athome.lu": "#FF9F40"
 };
