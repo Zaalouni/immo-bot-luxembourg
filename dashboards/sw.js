584fc65f364de9d4257afb1c228fb398f5feb1f1
zaalouni <nsaibi@gmail.com>
2026-02-20 20:09:43 +0100

    dashboard: mise a jour 2026-02-20 20:09

---
+++ dashboards/sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'immo-bot-202602202009';
+const ASSETS = ['./index.html', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+/* Network-first : donnees toujours fraiches, fallback cache si offline */
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(resp => {
+      const clone = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return resp;
+    }).catch(() => caches.match(e.request))
+  );
+});
