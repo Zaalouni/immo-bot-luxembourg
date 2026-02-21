ac7556be4be291d13de771922593190c3feae891
zaalouni <nsaibi@gmail.com>
2026-02-21 13:21:02 +0100

    dashboard: mise a jour 2026-02-21 13:21

--- dashboards/sw.js
+++ dashboards/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'immo-bot-202602211316';
+const CACHE = 'immo-bot-202602211321';
 const ASSETS = ['./index.html', './map.html', './icon.svg'];
 
 self.addEventListener('install', e => {
